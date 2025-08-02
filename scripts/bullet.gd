7e637e52ae1f484266b13c02c5656a95e6880c4c
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2025-08-02 13:56:32 -0400

    Added Screen Wrapping for the loop effect

---
+++ scripts/bullet.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+var pos:Vector2
+var rota:float
+var dir:float
+var SPEED = 210
+
+@onready var screen_size = get_viewport_rect().size
+
+#This script moves the bullet in the direction its rotated
+
+#This sets the position and rotation
+func _ready():
+	global_position=pos
+	global_rotation=rota
+
+#I believe this moves the bullet
+func _physics_process(delta):
+	velocity=Vector2(SPEED,0).rotated(dir)
+	move_and_slide()
+	screen_wrap()
+
+func screen_wrap(): 
+	var zoom = $"../Camera2D".zoom
+	var screen_size = get_viewport_rect().size
+	position.x = wrapf(position.x, 0, screen_size.x / zoom.x)
+	position.y = wrapf(position.y, 0, screen_size.y / zoom.y)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,52 @@
 extends CharacterBody2D
 
+var bullet_path=preload("res://scenes/bullet.tscn")
+const SPEED = 200.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var fire_rate = 0.2  # seconds between shots
+var time_since_last_shot = 0.0
 
+#@onready var screen_size = get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("shoot"):
+		time_since_last_shot += delta
+		if time_since_last_shot >= fire_rate:
+			fire()
+			time_since_last_shot = 0
+	else:
+		time_since_last_shot = fire_rate  # Reset to allow immediate fire on next press
+	
+	#move up and down
+	#i think this is inverse since the x y axis are inverse
+	var vertical := Input.get_axis("move_up", "move_down")
+	if vertical:
+		velocity.y = vertical * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# move left and right
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+	screen_wrap()
+
+func fire():
+	var projectile = bullet_path.instantiate()
+	projectile.dir = rotation
+	projectile.pos = $"Shooting Position".global_position
+	projectile.rota=global_rotation
+	get_parent().add_child(projectile)
+
+func screen_wrap(): 
+	var zoom = $"../Camera2D".zoom
+	var screen_size = get_viewport_rect().size
+	position.x = wrapf(position.x, 0, screen_size.x / zoom.x)
+	position.y = wrapf(position.y, 0, screen_size.y / zoom.y)
